782c4502e2a47a3d291873a1871cb7510ec95839
whwjdqja58@naver.com <whwjdqja58@naver.com>
2023-07-25 02:40:42 +0000

    commit test 03

--- test_query.sql
+++ test_query.sql
@@ -1,10 +1,16 @@
-SELECT * FROM 
-cat_golfzon_01.currency.br_total_exchange_rate A
-CROSS JOIN
-cat_golfzon_01.currency.br_total_exchange_rate B
-ON  A.CUR_UNIT = B.CUR_UNIT 
-AND A.STD_DT = B.STD_DT
-CROSS JOIN
-cat_golfzon_01.currency.br_total_exchange_rate C
-ON  A.CUR_UNIT = C.CUR_UNIT 
-AND A.STD_DT = C.STD_DT
+INSERT INTO cat_golfzon_01.currency.br_total_exchange_rate 
+SELECT RESULT,
+       CUR_UNIT,
+       TTB,
+       TTS,
+       DEAL_BAS_R,
+       BKPR,
+       YY_EFEE_R,
+       TEN_DD_EFEE_R,
+       KFTC_BKPR,
+       KFTC_DEAL_BAS_R,
+       CUR_NM,
+       STD_DT,
+       NOW() AS CRT_DT
+FROM cat_golfzon_01.currency.br_total_exchange_rate
+LIMIT 1
